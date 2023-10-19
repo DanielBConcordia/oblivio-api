db5a95fe9de9a1d35fca1710492cb38c21b6baf1
Guilherme <90399361+DanielBConcordia@users.noreply.github.com>
2023-10-19 14:20:11 -0300

    Update route

--- src/modules/cuidadores/cuidadorCases/createCuidador/createCuidadorCase.ts
+++ src/modules/cuidadores/cuidadorCases/createCuidador/createCuidadorCase.ts
@@ -6,17 +6,17 @@ import { AppError } from "../../../../errors/appError";
 export class CreateCuidadorCase {
     async execute({
         cpf, dtNascimento, email, telefone, telefoneReserva, nomeSocial, nome, senha, fotoPerfil, rua, cep, bairro, numeroCasa, cidade, uf, complementoCasa,pontoReferencia }: CreateCuidadorDTO ): Promise<Cuidador> {
-            //Verificar se o usuário já existe
+           // Verificar se o usuário já existe
             
-            // const cudadorAlreadyExists = await prisma.cuidador.findUnique({
-            //     where: {
-            //         email
-            //     }
-            // })
+            const cudadorAlreadyExists = await prisma.cuidador.findUnique({
+                where: {
+                    email
+                }
+            })
 
-            // if (cudadorAlreadyExists) {
-            //     throw new AppError("User already exists!");
-            // }
+            if (cudadorAlreadyExists) {
+                throw new AppError("User already exists!");
+            }
 
             // Criar o usuário
             const cuidador = await prisma.cuidador.create({
--- src/routes/cuidador.routes.ts
+++ src/routes/cuidador.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { CreateCuidadorController } from "../modules/cuidadores/cuidadorCases/createCuidador/createCuidadorController";
 import { GetCuidadorController } from "../modules/cuidadores/cuidadorCases/getCuidadorCase/getCuidadorController";
+import { Request, Response } from "express";
 const cuidadorRoutes = Router();
 
 const createUserController = new CreateCuidadorController();
 const getCuidador = new GetCuidadorController
 
-cuidadorRoutes.post("/cad", createUserController.handle);
+cuidadorRoutes.post("/cad", createUserController.handle, (req: Request, res: Response) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+});
 cuidadorRoutes.get("/list", getCuidador.handle);
 
 export { cuidadorRoutes };
