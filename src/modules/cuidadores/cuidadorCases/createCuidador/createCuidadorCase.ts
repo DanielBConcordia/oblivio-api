8e91bfbabb0d99c71d25e384cf5fcd9514d309c9
Guilherme <90399361+DanielBConcordia@users.noreply.github.com>
2023-10-19 15:01:02 -0300

    Update CreatCuidadorController

--- src/modules/cuidadores/cuidadorCases/createCuidador/createCuidadorCase.ts
+++ src/modules/cuidadores/cuidadorCases/createCuidador/createCuidadorCase.ts
@@ -8,15 +8,15 @@ export class CreateCuidadorCase {
         cpf, dtNascimento, email, telefone, telefoneReserva, nomeSocial, nome, senha, fotoPerfil, rua, cep, bairro, numeroCasa, cidade, uf, complementoCasa,pontoReferencia }: CreateCuidadorDTO ): Promise<Cuidador> {
            // Verificar se o usuário já existe
             
-            const cudadorAlreadyExists = await prisma.cuidador.findUnique({
-                where: {
-                    email
-                }
-            })
+            // const cudadorAlreadyExists = await prisma.cuidador.findUnique({
+            //     where: {
+            //         email
+            //     }
+            // })
 
-            if (cudadorAlreadyExists) {
-                throw new AppError("User already exists!");
-            }
+            // if (cudadorAlreadyExists) {
+            //     throw new AppError("User already exists!");
+            // }
 
             // Criar o usuário
             const cuidador = await prisma.cuidador.create({
